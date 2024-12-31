fb19df857b45526426c115db62d0cf79990ed903
0xmihirsahu <0xmihirsahu@gmail.com>
2024-12-31 21:29:24 +0530

    contract updated

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,14 @@
-// SPDX-License-Identifier: Unlicense
+ur// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
 contract Contract { 
-    public address admin;
+    string private message = "Hello World";
+    
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+    
+    function setMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
 }
